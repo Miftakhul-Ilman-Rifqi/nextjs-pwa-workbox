ee207de8b344f42789dab473d341c441f341fa30
Miftakhul Ilman Rifqi <bozzclax@gmail.com>
2025-04-21 13:45:46 +0700

    feat: enable dynamic revision generation for additional precache entries in service worker configuration

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,14 @@ import withSerwistInit from "@serwist/next";
 // You may want to use a more robust revision to cache
 // files more efficiently.
 // A viable option is `git rev-parse HEAD`.
-// const revision = crypto.randomUUID();
+const revision = crypto.randomUUID();
 
 const withSerwist = withSerwistInit({
     cacheOnNavigation: true,
     swSrc: "src/app/sw.ts",
     swDest: "public/sw.js",
     reloadOnOnline: true,
-    // additionalPrecacheEntries: [{ url: "/~offline", revision }],
+    additionalPrecacheEntries: [{ url: "/~offline", revision }],
 });
 
 /** @type {import("next").NextConfig} */
