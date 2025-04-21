b1193e18bf9933d0db16e29337f657d86c885f50
Miftakhul Ilman Rifqi <bozzclax@gmail.com>
2025-04-21 13:40:03 +0700

    refactor: remove unused revision generation and commented out precache entries in configuration

--- next.config.ts
+++ next.config.ts
@@ -4,22 +4,14 @@ import withSerwistInit from "@serwist/next";
 // You may want to use a more robust revision to cache
 // files more efficiently.
 // A viable option is `git rev-parse HEAD`.
-const revision = crypto.randomUUID();
+// const revision = crypto.randomUUID();
 
 const withSerwist = withSerwistInit({
     cacheOnNavigation: true,
     swSrc: "src/app/sw.ts",
     swDest: "public/sw.js",
     reloadOnOnline: true,
-    additionalPrecacheEntries: [
-        { url: "/~offline", revision },
-        { url: "/favicon/android-chrome-512x512.png", revision },
-        { url: "/favicon/android-chrome-192x192.png", revision },
-        { url: "/favicon/apple-touch-icon.png", revision },
-        { url: "/favicon/favicon-16x16.png", revision },
-        { url: "/favicon/favicon-32x32.png", revision },
-        { url: "/favicon/favicon.ico", revision },
-    ],
+    // additionalPrecacheEntries: [{ url: "/~offline", revision }],
 });
 
 /** @type {import("next").NextConfig} */
