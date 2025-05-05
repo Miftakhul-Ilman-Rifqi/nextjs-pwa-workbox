52e6099e85ec291e0b3cd5a5551c80033b5544af
Miftakhul Ilman Rifqi <bozzclax@gmail.com>
2025-05-05 16:30:47 +0700

    feat: remove Serwist integration and clean up service worker related files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,66 +1,8 @@
-// import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
-// import { ThemeProvider } from "@/components/providers/theme-provider";
-// import { Toaster } from "sonner";
-
-// const geistSans = Geist({
-//     variable: "--font-geist-sans",
-//     subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//     variable: "--font-geist-mono",
-//     subsets: ["latin"],
-// });
-
-// export const metadata: Metadata = {
-//     title: "Customer Management App",
-//     description: "Aplikasi manajemen data customer",
-//     manifest: "/manifest.json",
-//     themeColor: "#2563eb",
-//     viewport: "width=device-width, initial-scale=1, maximum-scale=1",
-//     appleWebApp: {
-//         capable: true,
-//         statusBarStyle: "default",
-//         title: "Customer Management App",
-//     },
-//     icons: {
-//         icon: "/vercel.svg",
-//         apple: "/vercel.svg", // menggunakan vercel.svg juga untuk apple
-//     },
-// };
-
-// export default function RootLayout({
-//     children,
-// }: Readonly<{
-//     children: React.ReactNode;
-// }>) {
-//     return (
-//         <html lang="en" suppressHydrationWarning>
-//             <body
-//                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-//             >
-//                 <ThemeProvider
-//                     attribute="class"
-//                     defaultTheme="system"
-//                     enableSystem
-//                     disableTransitionOnChange
-//                 >
-//                     {children}
-//                     <Toaster />
-//                 </ThemeProvider>
-//             </body>
-//         </html>
-//     );
-// }
-
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { Toaster } from "sonner";
-// import SWRegistrar from "../components/providers/sw-registrar";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -157,7 +99,6 @@ export default function RootLayout({
                     enableSystem
                     disableTransitionOnChange
                 >
-                    {/* <SWRegistrar /> */}
                     {children}
                     <Toaster />
                 </ThemeProvider>
